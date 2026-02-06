3fc7b87264e5d259cb6d8b6c7f4ce4db1f2ea2d3
AdamCarter27 <adamcarter27@icloud.com>
2026-02-05 16:17:00 -0800

    added functionality for date filter

--- app/(tabs)/newsletter.tsx
+++ app/(tabs)/newsletter.tsx
@@ -3,14 +3,14 @@ import React, { useState } from "react";
 import { NewsUpdate } from "../../components/NewsUpdate";
 import { WebView } from "react-native-webview";
 import DropDownPicker from 'react-native-dropdown-picker'
-import { Ionicons } from "@expo/vector-icons"; // for "filter-outline" on dropdown
+import { Ionicons } from "@expo/vector-icons"; //"filter-outline" dropdown and "menu-outline" menu icon
 
 const NEWSLETTERS = [
   {
     id: "5",
     title: "Environteers Weekly Update",
     editionNumber: 5,
-    date: "01/12/2026",
+    date: "02/04/2026",
     link: "https://mailchi.mp/f61e915c4e8e/environteers-weekly-update?e=298d639b33",
     previewImage:
       "https://mcusercontent.com/37708d1720fdc287c7e9795e8/images/26f6c4d7-a555-2c70-cc48-17dbcf267087.jpeg",
@@ -29,6 +29,23 @@ const NEWSLETTERS = [
 const includesText = (str: string, search: string) =>
   str.toLowerCase().includes(search.toLowerCase());
 
+const parseDate = (dateStr: string) =>{
+  const [month, day, year] = dateStr.split('/').map(Number);
+  return new Date(year, month - 1, day);
+}
+const includesDate = (dateStr: string, filter: 'week' | '2weeks' | 'month' | 'all') => {
+  if (filter === 'all') return true;
+  const date = parseDate(dateStr);
+  const now = new Date();
+  const diffTime = Math.abs(now.getTime() - date.getTime());
+  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+  if (filter === 'week') return diffDays <= 7;
+  if (filter === '2weeks') return diffDays <= 14;
+  if (filter === 'month') return diffDays <= 30;
+  return false;
+}
+
+
 export default function Newsletter() {
   const [activeUrl, setActiveUrl] = useState<string | null>(null);
   const [searchText, setSearchText] = useState("");
@@ -37,7 +54,7 @@ export default function Newsletter() {
 
 
   const filteredNewsletters = NEWSLETTERS.filter((n) =>
-    includesText(n.title, searchText)
+    (includesText(n.title, searchText) || includesText(n.editionNumber.toString(), searchText))&& (includesDate(n.date, filterDateLength))
   );
   //displaying webview of a newsletter
   if (activeUrl) {
@@ -60,8 +77,9 @@ export default function Newsletter() {
   }
   //displaying list of newsletters
   return (
-    <View>
-      <Text style = {{marginTop: 20, marginBottom :16, marginLeft : 12, fontWeight: "bold", fontSize: 30}}> Weekly Updates</Text>
+    <View style={{ flex: 1 }}>
+      <Ionicons name = "menu-outline" size = {30} style = {{marginTop: 8, marginLeft: 8}} />
+      <Text style = {{marginTop: 8, marginBottom :12, marginLeft : 12, fontWeight: "bold", fontSize: 30}}> Weekly Updates</Text>
       
         <TextInput
           placeholder="Search newsletters"
@@ -71,14 +89,14 @@ export default function Newsletter() {
           onChangeText={setSearchText}
         />
 
-      {/* dropdown filter */}
-      <View style = {{flexDirection: 'row'}}>
+      <View style = {{flexDirection: 'row', marginLeft: 12}}>
         <Ionicons style = {{marginTop: 14, marginRight: 4, marginLeft: 4}}name = "filter-outline" size = {24}/>
         <DropDownPicker
           open={filterOpen}
           setOpen = {setFilterOpen}
           value={filterDateLength}
           setValue={setFilterDateLength}
+  
           items={[
             { label: 'Past Week', value: 'week' },
             { label: 'Past 2 Weeks', value: '2weeks' },
@@ -115,7 +133,7 @@ export default function Newsletter() {
 const styles = StyleSheet.create({
   search: {
     borderWidth: 1,
-    width: "95%",
+    width: "90%",
     alignSelf: "center",
     borderColor: "#151414",
     borderRadius: 24,
@@ -128,7 +146,6 @@ const styles = StyleSheet.create({
     borderRadius: 24,
     borderWidth: 1,
     borderColor: "#151414",
-    maxHeight: 160,
   },
    filter: {
       width: "50%",
@@ -136,7 +153,5 @@ const styles = StyleSheet.create({
       borderWidth: 1,
       borderColor: "#151414",
       backgroundColor: "transparent",
-      paddingHorizontal: 16,
-      paddingVertical: 10,
     }}
 );
