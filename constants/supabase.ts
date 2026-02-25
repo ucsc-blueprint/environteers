7d9a58269b85a3875af017c5997ed3253de43601
Nicholas Tran <ntran62@ucsc.edu>
2026-02-24 18:22:15 -0800

    revert back to removing user metadata version

--- constants/supabase.ts
+++ constants/supabase.ts
@@ -1,47 +1,6 @@
 import 'react-native-url-polyfill/auto';
 import { createClient } from '@supabase/supabase-js';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import * as SecureStore from 'expo-secure-store';
-import * as aesjs from 'aes-js';
-import 'react-native-get-random-values';
-
-class LargeSecureStore {
-
-    private async _encrypt(key: string, value: string) {
-        const encryptionKey = crypto.getRandomValues(new Uint8Array(32));
-        const cipher = new aesjs.ModeOfOperation.ctr(encryptionKey, new aesjs.Counter(1));
-        const encryptedBytes = cipher.encrypt(aesjs.utils.utf8.toBytes(value));
-        const encryptedHex = aesjs.utils.hex.fromBytes(encryptionKey);
-        await SecureStore.setItemAsync(key, encryptedHex);
-        return aesjs.utils.hex.fromBytes(encryptedBytes);
-    }
-
-    private async _decrypt(key: string, value: string) {
-        const encryptionKeyHex = await SecureStore.getItemAsync(key);
-        if (!encryptionKeyHex) {
-            throw new Error('Encryption key not found in SecureStore');
-        }
-        const cipher = new aesjs.ModeOfOperation.ctr(aesjs.utils.hex.toBytes(encryptionKeyHex), new aesjs.Counter(1));
-        const decryptedBytes = cipher.decrypt(aesjs.utils.hex.toBytes(value));
-        return aesjs.utils.utf8.fromBytes(decryptedBytes);
-    }
-
-    async getItem(key: string) {
-        const encryptedValue = await AsyncStorage.getItem(key);
-        if (!encryptedValue) { return encryptedValue; }
-        return await this._decrypt(key, encryptedValue);
-    }
-
-    async removeItem(key: string) {
-        await AsyncStorage.removeItem(key);
-        await SecureStore.deleteItemAsync(key);
-    }
-
-    async setItem(key: string, value: string) {
-        const encryptedValue = await this._encrypt(key, value);
-        await AsyncStorage.setItem(key, encryptedValue);
-    }
-}
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
 const supabasePublishableKey = process.env.EXPO_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
@@ -52,11 +11,34 @@ if (!supabaseUrl || !supabasePublishableKey) {
     );
 }
 
+function removeUserMetaData(itemValue: string) {
+    let parsedItemValue = JSON.parse(itemValue);
+
+    if (parsedItemValue) {
+        delete parsedItemValue.user?.identities;
+        delete parsedItemValue.user?.user_metadata;
+    }
+
+    return JSON.stringify(parsedItemValue);
+}
+
+const ExpoSecureStoreAdapter = {
+    getItem: (key: string) => {
+        return SecureStore.getItemAsync(key);
+    },
+    setItem: (key: string, value: string) => {
+        return SecureStore.setItemAsync(key, removeUserMetaData(value));
+    },
+    removeItem: (key: string) => {
+        return SecureStore.deleteItemAsync(key);
+    },
+};
+
 export const supabase = createClient(
     supabaseUrl,
     supabasePublishableKey, {
         auth: {
-            storage: new LargeSecureStore,
+            storage: ExpoSecureStoreAdapter,
             autoRefreshToken: true,
             persistSession: true,
             detectSessionInUrl: false,
