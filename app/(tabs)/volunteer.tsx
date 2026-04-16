2ec9fae5a92adf61e6368f1690e1b0a094d9eab8
Nicholas Tran <ntran62@ucsc.edu>
2026-04-15 17:01:40 -0700

    spacing fix

--- app/(tabs)/volunteer.tsx
+++ app/(tabs)/volunteer.tsx
@@ -15,18 +15,18 @@ import { useRefresh } from "@/context/RefreshContext";
 export type CardProps = InPersonCardDataProps | OnlineCardDataProps | EventCardDataProps;
 
 const getDistance = (lat1: number, lon1: number, lat2: number, lon2: number) => {
-    const R = 3958.8;
-    const dLat = (lat2 - lat1) * Math.PI / 180;
-    const dLon = (lon2 - lon1) * Math.PI / 180;
+  const R = 3958.8;
+  const dLat = (lat2 - lat1) * Math.PI / 180;
+  const dLon = (lon2 - lon1) * Math.PI / 180;
 
-    const a =
-      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-      Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
-      Math.sin(dLon / 2) * Math.sin(dLon / 2);
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    
-    return R * c;
-  };
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
+    Math.sin(dLon / 2) * Math.sin(dLon / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  
+  return R * c;
+};
 
 export default function Volunteer() {
   const { user } = useAuth();
