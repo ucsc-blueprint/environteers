b1c736dee0906d44c62a789c3dad680894d908e6
Vibhi Reddy <vibhar789@gmail.com>
2026-04-22 16:38:09 -0700

    fixed add eco action button

--- app/(tabs)/volunteer.tsx
+++ app/(tabs)/volunteer.tsx
@@ -194,11 +194,38 @@ export default function Volunteer() {
           <EcoFeed key={`${card.cardType}-${card.cardInfo.id}`} card={card} />
         ))}
       </ScrollView>
+      {isAdmin ? (
+        <>
+          <Pressable
+            style={styles.addButton}
+            onPress={() => setShowAddMenu(true)}>
+            <Text style={styles.addButtonText}>+ Add</Text>
+          </Pressable>
+
+          <Modal visible={showAddMenu} transparent animationType="fade" onRequestClose={() => setShowAddMenu(false)}>
+            <Pressable style={styles.backdrop} onPress={() => setShowAddMenu(false)}>
+              <Pressable style={styles.popup} onPress={() => { }}>
+                <Text style={styles.popupTitle}>Add something new</Text>
+                <Pressable style={styles.popupButton} onPress={() => goToAddForm("event")}>
+                  <Text>Add Event</Text>
+                </Pressable>
+                <Pressable style={styles.popupButton} onPress={() => goToAddForm("in-person")}>
+                  <Text>Add In-Person Eco-Action</Text>
+                </Pressable>
+                <Pressable style={styles.popupButton} onPress={() => goToAddForm("online")}>
+                  <Text>Add Online Eco-Action</Text>
+                </Pressable>
+              </Pressable>
+            </Pressable>
+          </Modal>
+        </>
+      ) : (
         <View style={styles.mapBackground}>
           <Pressable onPress={() => router.push('/(tabs)/map')}>
             <MaterialCommunityIcons name="map" size={30} color={'#0282D3'} />
           </Pressable>
         </View>
+      )}
     </LinearGradient>
   );
 }
@@ -220,5 +247,39 @@ const styles = StyleSheet.create({
     bottom: 10,
     right: 20,
     boxShadow: '0px 0px 10px 0px #0282D333',
-  }
+  },
+
+  addButton: {
+    position: 'absolute',
+    bottom: 15,
+    right: 20,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 12,
+    zIndex: 10,
+    backgroundColor: '#94C153',
+    height: 45,
+    width: 80,
+  },
+  backdrop: {
+    flex: 1, backgroundColor: 'rgba(0,0,0,0.35)',
+    justifyContent: 'center', alignItems: 'center', padding: 20,
+  },
+  popup: {
+    width: '100%', backgroundColor: 'white',
+    borderRadius: 16, padding: 16, gap: 12,
+  },
+
+  popupTitle: { fontSize: 18, fontWeight: '600', marginBottom: 6 },
+  popupButton: {
+    paddingVertical: 14, paddingHorizontal: 12,
+    borderRadius: 12, backgroundColor: '#F2F2F2',
+  },
+
+   addButtonText: {
+    color: 'white',
+    fontWeight: '600',
+    fontSize: 16,
+  },
 });
